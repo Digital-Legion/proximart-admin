c7a04f79145e646b37a1aa9f4e93542f1540b40f
Nadir <chipchroumme@gmail.com>
2021-07-18 13:25:21 +0400

    clean app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+// default
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// styles
+import './assets/styles/app.scss'
+
+// plugins/modules
+import axios from 'axios'
+
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL || ''
 
 new Vue({
   router,
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="index-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
